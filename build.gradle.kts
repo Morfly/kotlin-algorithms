a226c208e3e8a1ab04788180667eb33fd8572403
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-12-13 19:01:24 -0800

    restructured the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,3 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    application
-}
-
-group = "io.morfly"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(8)
-}
-
-application {
-    mainClass.set("MainKt")
+    kotlin("jvm") version "1.9.0" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,9 +4,21 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+    }
+}
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "algorithms"
+rootProject.name = "kotlin-algorithms"
+
+include(
+    ":foundation",
+    ":exercises",
+    ":leetcode"
+)
