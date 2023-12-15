1cdcfafd150bc14c85ffdab8127113ddd14e8f63
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-12-14 20:59:39 -0800

    add leetcode two sum

---
+++ src/leetcode/1-two-sum.kt
@@ -0,0 +1,23 @@
+package leetcode
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = mutableMapOf<Int, Int>()
+
+    for (i in nums.indices) {
+        val complement = map[target - nums[i]]
+        if (complement != null) {
+            return intArrayOf(complement, i)
+        }
+        map[nums[i]] = i
+    }
+    return intArrayOf()
+}
+
+fun main() {
+    val nums = intArrayOf(2, 7, 11, 15)
+
+    val result = twoSum(nums, target = 9).toList()
+
+    println(result)
+    require(result == listOf(0, 1))
+}
