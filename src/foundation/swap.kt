d27aa592b5adf2c2de9d51b540b7cf5de894ee99
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2024-01-03 13:25:33 -0800

    add sample code for swaps

--- src/foundation/swap.kt
+++ src/foundation/swap.kt
@@ -3,3 +3,14 @@ package foundation
 fun IntArray.swap(i: Int, j: Int) {
     this[i] = this[j].also { this[j] = this[i] }
 }
+
+fun main() {
+    val array = intArrayOf(1, 2, 3, 4, 5)
+
+    array.swap(i = 0, j = 3)
+
+    val result = array.toList()
+
+    println(result)
+    require(result == listOf(4, 2, 3, 1, 5))
+}
