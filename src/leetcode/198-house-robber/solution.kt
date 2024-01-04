d452fb1d73c0c41f9c88afce4e51bcb9d2c603bb
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2024-01-03 23:56:12 -0800

    add leetcode 198

---
+++ src/leetcode/198-house-robber/solution.kt
@@ -0,0 +1,23 @@
+package leetcode.`198-house-robber`
+
+// https://leetcode.com/problems/house-robber
+fun rob(nums: IntArray): Int {
+    var rob1 = 0
+    var rob2 = 0
+
+    for (num in nums) {
+        val temp = maxOf(num + rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+    }
+    return rob2
+}
+
+fun main() {
+    val nums = intArrayOf(1, 2, 3, 1)
+
+    val result = rob(nums)
+
+    println(result)
+    require(result == 4)
+}
