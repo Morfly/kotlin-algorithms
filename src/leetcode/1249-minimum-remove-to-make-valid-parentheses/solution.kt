28e1ca9c7a9461dd18a52a89f197688ce6abc2ca
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2024-02-28 22:01:22 -0800

    add leetcode 1249

---
+++ src/leetcode/1249-minimum-remove-to-make-valid-parentheses/solution.kt
@@ -0,0 +1,36 @@
+@file:Meta(companies = [Company.Meta], topics = [Topic.Stack])
+
+package leetcode.`1249-minimum-remove-to-make-valid-parentheses`
+
+fun minRemoveToMakeValid(s: String): String {
+    val open = ArrayDeque<Int>()
+    val indicesToRemove = mutableSetOf<Int>()
+
+    for (i in s.indices) {
+        if (s[i] == '(') {
+            open += i
+        } else if (s[i] == ')') {
+            if (open.isEmpty()) {
+                indicesToRemove += i
+            } else {
+                open.removeLast()
+            }
+        }
+    }
+    indicesToRemove += open
+
+    val result = StringBuilder()
+    for (i in s.indices) {
+        if (i !in indicesToRemove) {
+            result.append(s[i])
+        }
+    }
+    return result.toString()
+}
+
+fun main() {
+    val result = minRemoveToMakeValid("lee(t(c)o)de)")
+
+    println(result)
+    require(result == "lee(t(c)o)de")
+}
--- tools/src/kotlin/Meta.kt
+++ tools/src/kotlin/Meta.kt
@@ -16,4 +16,5 @@ enum class Topic {
     BinarySearch,
     BinaryTree,
     BreadthFirstSearch,
+    Stack
 }
