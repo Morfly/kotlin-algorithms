9a183ebe7861b1fe9c01a60b34b2380ca36cf869
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2024-03-02 21:45:48 -0800

    add leetcode 191

---
+++ src/leetcode/191-number-of-1-bits/solution.kt
@@ -0,0 +1,22 @@
+@file:Problem(topics = [Topic.BitManipulation])
+
+package leetcode.`191-number-of-1-bits`
+
+fun hammingWeight(n: Int): Int {
+    var bits = 0
+    var a = n
+    while (a != 0) {
+        if (a and 1 == 1) bits++
+        a = a ushr 1
+    }
+    return bits
+}
+
+fun main() {
+    val input = 0b01000000000000000000000000001011
+
+    val result = hammingWeight(input)
+
+    println(result)
+    require(result == 4)
+}
