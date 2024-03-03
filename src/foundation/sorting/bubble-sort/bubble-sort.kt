7a6a132ae49aa04ae0191617f339768c23703ace
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2024-03-02 19:35:40 -0800

    update bubble sort

--- src/foundation/sorting/bubble-sort/bubble-sort.kt
+++ src/foundation/sorting/bubble-sort/bubble-sort.kt
@@ -1,21 +1,27 @@
+@file:SortingAlgorithm(Stable(true), InPlace(true), Comparative(true), Recursive(false))
+
 package foundation.sorting.`bubble-sort`
 
 import io.morfly.algorithms.tools.isSorted
 
-fun IntArray.bubbleSort() {
-    for (i in 0..<lastIndex) {
-        for (j in 0..<lastIndex - i) {
-            if (this[j] > this[j + 1]) {
-                this[j] = this[j + 1].also { this[j + 1] = this[j] }
+fun bubbleSort(array: IntArray) {
+    for (i in 0..<array.lastIndex) {
+        for (j in 0..<array.lastIndex - i) {
+            if (array[j] > array[j + 1]) {
+                array.swap(j, j + 1)
             }
         }
     }
 }
 
+fun IntArray.swap(i: Int, j: Int) {
+    this[i] = this[j].also { this[j] = this[i] }
+}
+
 fun main() {
     val array = intArrayOf(64, 34, 25, 12, 22, 11, 90)
 
-    array.bubbleSort()
+    bubbleSort(array)
 
     println(array.joinToString())
     require(array.isSorted())
