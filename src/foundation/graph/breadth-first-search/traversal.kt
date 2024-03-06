00a0fdde14b643883572dfc860aaa1ab8e3c82cf
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2024-03-06 00:45:56 -0800

    update graph bfs traversal algorithm

--- src/foundation/graph/breadth-first-search/traversal.kt
+++ src/foundation/graph/breadth-first-search/traversal.kt
@@ -4,14 +4,12 @@ package foundation.graph.`breadth-first-search`.traversal
 
 typealias Graph<T> = Map<T, List<T>>
 
-typealias Queue<T> = ArrayDeque<T>
-
 fun <T> Graph<T>.traverse(root: T): List<T> {
     val graph = this
     require(root in graph) { "Invalid root vertex '$root'" }
 
     val explored = linkedSetOf<T>()
-    val queue = Queue<T>()
+    val queue = ArrayDeque<T>()
     queue += root
 
     while (queue.isNotEmpty()) {
