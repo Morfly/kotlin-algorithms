c6f6d9abdd65887130778c5d5c5b037b5a6fc882
Morfly <pavlo.stavytskyi@gmail.com>
2024-04-07 18:49:35 -0700

    refactor Kruskal MST algorithm sorting condition

--- src/foundation/minimum-spanning-tree/kruskal/kruskal-mst.kt
+++ src/foundation/minimum-spanning-tree/kruskal/kruskal-mst.kt
@@ -23,30 +23,32 @@ fun minSpanningTree(edges: List<Edge>): List<Edge> {
 
 fun main() {
     val edges = listOf(
-        Edge(src = "A", dest = "D", weight = 1),
-        Edge(src = "A", dest = "B", weight = 2),
-        Edge(src = "B", dest = "D", weight = 3),
-        Edge(src = "E", dest = "D", weight = 9),
-        Edge(src = "E", dest = "A", weight = 4),
-        Edge(src = "B", dest = "C", weight = 3),
-        Edge(src = "C", dest = "D", weight = 5),
-        Edge(src = "B", dest = "F", weight = 7),
-        Edge(src = "C", dest = "F", weight = 8),
+        edge(src = "A", dest = "D", weight = 1),
+        edge(src = "A", dest = "B", weight = 2),
+        edge(src = "B", dest = "D", weight = 3),
+        edge(src = "E", dest = "D", weight = 9),
+        edge(src = "E", dest = "A", weight = 4),
+        edge(src = "B", dest = "C", weight = 3),
+        edge(src = "C", dest = "D", weight = 5),
+        edge(src = "B", dest = "F", weight = 7),
+        edge(src = "C", dest = "F", weight = 8),
     )
 
     val result = minSpanningTree(edges)
     val totalWeight = result.sumOf { it.weight }
 
-    println(result)
+    println(result.joinToString("\n"))
     println(totalWeight)
     require(
         result == listOf(
-            Edge(src = "A", dest = "D", weight = 1),
-            Edge(src = "A", dest = "B", weight = 2),
-            Edge(src = "B", dest = "C", weight = 3),
-            Edge(src = "E", dest = "A", weight = 4),
-            Edge(src = "B", dest = "F", weight = 7)
+            edge(src = "A", dest = "D", weight = 1),
+            edge(src = "A", dest = "B", weight = 2),
+            edge(src = "B", dest = "C", weight = 3),
+            edge(src = "E", dest = "A", weight = 4),
+            edge(src = "B", dest = "F", weight = 7)
         )
     )
     require(totalWeight == 17)
 }
+
+fun edge(src: String, dest: String, weight: Int) = Edge(src, dest, weight)
